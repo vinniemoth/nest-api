b485a1bbb7f2deed1f1d3e6815a55b326e0bd74a
Vinicius <vicimon15@gmail.com>
2024-09-08 22:28:56 -0300

    Adição dos imports restantes no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
 import { QuadrinhoModule } from './quadrinho/quadrinho.module';
+import { EditoraModule } from './editora/editora.module';
+import { AutorModule } from './autor/autor.module';
+
 
 @Module({
-  imports: [AdminModule, QuadrinhoModule],
+  imports: [AdminModule, QuadrinhoModule, EditoraModule, AutorModule],
   controllers: [],
   providers: [],
 })
