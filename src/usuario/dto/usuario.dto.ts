a9361dc577ba75a0746720df87f1bcb649c2c448
Vinnie Moth <vinniemoth@gmail.com>
2024-09-13 12:50:42 -0300

    Adicionada a validação de senha forte

--- src/usuario/dto/usuario.dto.ts
+++ src/usuario/dto/usuario.dto.ts
@@ -1,27 +1,40 @@
 //DTO responsável por receber dados de criação de um novo usuário
 //DTO é "data transfer object" ou objeto de transferencia de dados, ou seja, é um tipo de classe para transferir dados
 
-import { IsEmail, IsNotEmpty, IsNumber, IsString, MinLength } from "class-validator";
-import { EmailUnico } from "../validacao/email-unico.validator";
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { EmailUnico } from '../validacao/email-unico.validator';
+import {
+  SenhaForte,
+  StrongPassValidator,
+} from '../validacao/strongpass.validator';
 
-export class criaUsuarioDTO{
-    @IsString()
-    @IsNotEmpty({message: "nome não pode ser vazio"})
-    nome: string;
+export class criaUsuarioDTO {
+  @IsString()
+  @IsNotEmpty({ message: 'nome não pode ser vazio' })
+  nome: string;
 
-    @EmailUnico({message: "Email repetido"})
-    @IsEmail(undefined, {message: "email inválido"})
-    email: string;
+  @EmailUnico({ message: 'Email repetido' })
+  @IsEmail(undefined, { message: 'email inválido' })
+  email: string;
 
-    @MinLength(6, {message: "senha deve ter no minimo 6 digitos"})
-    senha:string;
-    
-    @IsNumber()
-    idade: number;
+  @MinLength(6, { message: 'senha deve ter no minimo 6 digitos' })
+  @SenhaForte({
+    message: 'Senha deve ter complexidade maior. Está muito fraca',
+  })
+  senha: string;
 
-    @IsString()
-    cidade: string;
+  @IsNumber()
+  idade: number;
 
-    @IsString()
-    telefone: string;
+  @IsString()
+  cidade: string;
+
+  @IsString()
+  telefone: string;
 }
