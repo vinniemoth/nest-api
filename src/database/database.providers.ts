ac9ee1a1779f5837cf647289be2784d13db0542d
Vinnie Moth <vinniemoth@gmail.com>
2024-10-23 18:04:58 -0300

    Synchronize setado para false

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -12,7 +12,7 @@ export const databaseProviders = [
         password: '',
         database: 'sudocomics',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
+        synchronize: false,
       });
 
       return dataSource.initialize();
